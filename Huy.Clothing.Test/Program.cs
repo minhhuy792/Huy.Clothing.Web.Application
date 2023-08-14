0fb1cf5b63a2028cdad5ae79ec8fa845791e4fc7
Co Ngoc Minh Huy <huy09842@gmail.com>
2023-08-14 19:51:13 +0700

    commit test

--- Huy.Clothing.Test/Program.cs
+++ Huy.Clothing.Test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Huy.Clothing.Application.Interfaces.Common;
+using Huy.Clothing.Application.Interfaces.Repositories;
 using Huy.Clothing.Infrastructrure.Context;
+using Huy.Clothing.Infrastructrure.Repositoties;
 using Huy.Clothing.Shared;
 namespace Huy.Clothing.Test;
 
@@ -7,17 +9,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        /*
-        SolidStoreContext db = new();
-        var products = db.Products.ToList();
-        foreach ( var product in products )
-        {
-            Console.WriteLine(product.ProductId + " " + product.ProductName);
-        }
-        */
-        //anonymous methods
+        //1.lay ra mot mang cac products => test xem repository chay the nao ?
         DbFactoryContext dbContext = new DbFactoryContext(() => new SolidStoreContext());
+        //2.init ApplicationDbContext
         IApplicationDbContext db = new ApplicationDbContext(dbContext);
+        //3.Khoi tao repository
+        IRepository<Product> productRepository = new Repository<Product>(db);
+        var data = productRepository.Find(1);
+        Console.WriteLine($"Product ID: {data.ProductId}, Product Name: {data.ProductName}");
         Console.ReadLine();
 
     }
