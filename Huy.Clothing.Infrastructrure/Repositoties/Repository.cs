ef9d927c3a0a24f2513b4b7c4fa7a708dd9590d2
Co Ngoc Minh Huy <huy09842@gmail.com>
2023-08-14 18:56:36 +0700

    commit repository

--- Huy.Clothing.Infrastructrure/Repositoties/Repository.cs
+++ Huy.Clothing.Infrastructrure/Repositoties/Repository.cs
@@ -1,10 +1,5 @@
 ﻿using Huy.Clothing.Application.Interfaces.Repositories;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Huy.Clothing.Infrastructrure.Repositoties;
 public class Repository<T> : IRepository<T> where T : class
