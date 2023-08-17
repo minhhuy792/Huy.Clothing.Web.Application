efed786e03cd5b9341c5b4e12ade2a129d43e824
Co Ngoc Minh Huy <huy09842@gmail.com>
2023-08-17 20:25:44 +0700

    commit UnitOfWork

--- Huy.Clothing.Infrastructrure/GlobalUsing.cs
+++ Huy.Clothing.Infrastructrure/GlobalUsing.cs
@@ -1,8 +1,14 @@
 ﻿global using System;
+global using System.Data;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+
 global using Huy.Clothing.Application.Interfaces.Repositories;
 global using Huy.Clothing.Application.Interfaces.Common;
 global using Huy.Clothing.Infrastructrure.Context;
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.EntityFrameworkCore.Metadata.Internal;
--- Huy.Clothing.Infrastructrure/Repositoties/UnitOfWork.cs
+++ Huy.Clothing.Infrastructrure/Repositoties/UnitOfWork.cs
@@ -1,5 +1,97 @@
-﻿namespace Huy.Clothing.Infrastructrure.Repositoties;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class UnitOfWork
+namespace Huy.Clothing.Infrastructrure.Repositoties;
+
+public class UnitOfWork : IUnitOfWork
 {
+    // caching repository after initialized turn (Redis)
+    private Dictionary<string, object> Repositories {  get;}
+    private IDbContextTransaction _transaction;
+    private IsolationLevel? _isolationLevel;
+    public IApplicationDbContext ApplicationDbContext { get; private set; }
+
+    public UnitOfWork(IApplicationDbContext applicationDbContext)
+    {
+        ApplicationDbContext = applicationDbContext;
+        this.Repositories = new Dictionary<string, dynamic>();
+    }
+
+    public async Task BeginTransactionAsync()
+    {
+        await StartNewTransactTionIfNeeded();
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        await ApplicationDbContext.DbContext.SaveChangesAsync();
+        if (_transaction is null) return;
+        await _transaction.CommitAsync();
+        await _transaction.DisposeAsync(); //giai phong transaction
+        _transaction = null;
+    }
+    /// <summary>
+    /// Return a instance of repository 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public IRepository<T> Repository<T>() where T : class
+    {
+        var type = typeof(T); //lay kieu cua entity nao do 
+        var typeName = type.Name;
+        //lock: giup dong bo hoa cac loi goi tao Repository
+        lock (Repositories)
+        {
+            if (Repositories.ContainsKey(typeName))
+            {
+                return (IRepository<T>)Repositories[typeName];
+            }
+            var repository = new Repository<T>(ApplicationDbContext);
+            //add vao cache
+            Repositories.Add(typeName, repository);
+            //return
+            return repository;
+
+        }
+
+    }
+
+    public async Task RollbackTransactionAsync()
+    {
+        if (_transaction == null) return;
+        await _transaction.RollbackAsync();
+        await _transaction.DisposeAsync(); //giai phong transaction
+        _transaction = null;
+    }
+
+    public async Task<int> saveChangesAsync(CancellationToken cancellationToken = default)
+    => await ApplicationDbContext.DbContext.SaveChangesAsync(cancellationToken);
+    /// <summary>
+    /// Begin a transaction
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private async Task StartNewTransactTionIfNeeded()
+    {
+        if(_transaction == null)
+        {
+            _transaction = _isolationLevel.HasValue ?
+                await ApplicationDbContext.DbContext.Database.BeginTransactionAsync(_isolationLevel.GetValueOrDefault()):
+                await ApplicationDbContext.DbContext.Database.BeginTransactionAsync();
+        }
+    }
+
+    public void Dispose()
+    {
+        //nothing: connection database, nothing object
+        if(ApplicationDbContext == null) return;
+        //close connect
+        if(ApplicationDbContext.DbContext.Database.GetDbConnection().State == ConnectionState.Open)
+        {
+            ApplicationDbContext.DbContext.Database.GetDbConnection().Close();
+        }
+        ApplicationDbContext.DbContext.Dispose();
+        ApplicationDbContext = null;
+        GC.SuppressFinalize(this); //nothing all object (unitofwork)
+    }
 }
